86f3108ac49d507b30f3c265a6474fc43166f6ba
J. Cohen <hostelmarketing@gmail.com>
2020-04-15 22:14:07 -0700

    Render square on screen

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ bool initialize_window(void) {
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
     }
-    // TODO: Create SDL window
+    // Create SDL window
     window =
         SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                          800, 600, SDL_WINDOW_BORDERLESS);
@@ -22,8 +22,8 @@ bool initialize_window(void) {
         return false;
     }
 
-    // TODO: Create SDL renderer
-    renderer = SDL_CreateRenderer(Window, -1, 0);
+    // Create SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
         fprintf(stderr, "Error creating SDL renderer.\n");
         return false;
@@ -32,9 +32,46 @@ bool initialize_window(void) {
     return true;
 }
 
+void setup(void) {
+    // TODO:
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) is_running = false;
+            break;
+    }
+}
+
+void update(void) {
+    // TODO:
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
 int main(void) {
-    /* TODO: Create an SDL window */
+    /* Create an SDL window */
     is_running = initialize_window();
+
+    setup();
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
 
     return 0;
 }
