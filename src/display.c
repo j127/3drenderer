00c2a9a3354e9ab59ee7ba789c6e583ce293ea0e
J. Cohen <hostelmarketing@gmail.com>
2020-04-17 02:10:22 -0700

    Update draw_rect to use draw_pixel and input check

--- src/display.c
+++ src/display.c
@@ -66,7 +66,7 @@ void draw_grid(int spacing) {
  * Draw a pixel
  */
 void draw_pixel(int x, int y, uint32_t color) {
-    if (x < window_width && y < window_height) {
+    if (x >= 0 && x < window_width && y >= 0 && y < window_height) {
         color_buffer[(window_width * y) + x] = color;
     }
 }
@@ -79,7 +79,7 @@ void draw_rect(int x, int y, int width, int height, uint32_t color) {
         for (int j = 0; j < height; j++) {
             int cur_x = x + i;
             int cur_y = y + j;
-            color_buffer[(window_width * cur_y) + cur_x] = color;
+            draw_pixel(cur_x, cur_y, color);
         }
     }
 }
